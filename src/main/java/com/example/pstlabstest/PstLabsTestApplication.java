a1b2f7b4936b1b76e32a78ebaace85b6b5acfdc1
Anton Novosad <noobito.2011@gmail.com>
2021-10-22 00:07:20 +0300

    added Security and SwaggerConfiguration

---
+++ src/main/java/com/example/pstlabstest/PstLabsTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.pstlabstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PstLabsTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PstLabsTestApplication.class, args);
+    }
+
+}
