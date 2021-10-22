4f8b148e2762ea103ad3d9d9187f96648829008d
Anton Novosad <noobito.2011@gmail.com>
2021-10-22 11:52:15 +0300

    added role repository, changed user repository

--- src/main/java/com/example/pstlabstest/repository/UserRepository.java
+++ src/main/java/com/example/pstlabstest/repository/UserRepository.java
@@ -11,5 +11,7 @@ import java.util.Optional;
 @Transactional(readOnly = true)
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+    boolean existsByPhone(String phone);
     boolean existsById(long id);
 }
