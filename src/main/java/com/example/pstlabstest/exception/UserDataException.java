b4e354551c63792297866f3db7958b51ef088c5c
Anton Novosad <noobito.2011@gmail.com>
2021-10-22 11:52:30 +0300

    added exception

---
+++ src/main/java/com/example/pstlabstest/exception/UserDataException.java
@@ -0,0 +1,8 @@
+package com.example.pstlabstest.exception;
+
+public class UserDataException extends RuntimeException{
+
+    public UserDataException(String message) {
+        super(message);
+    }
+}
